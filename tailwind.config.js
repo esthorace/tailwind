32ec17869b5d7e0042951e0351b1d8d842e61426
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-02-05 12:14:12 -0300

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/*.html"],
+  content: ["src/*.html", "src/logo-app.svg"],
   theme: {
     extend: {},
   },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
+    daisyui: {
+      themes: ["light", "dark",
+        {
+          light: {
+            ...require("daisyui/src/theming/themes")["light"],
+            "primary": "#fb923c",
+            "secondary": "#55b3df",
+            "accent": "#a3e635",
+            "neutral": "#1f2937",
+            "base": "#e4f4ff",
+            "base-300": "#d5eeff",
+            "info": "#c084fc",
+            "success": "#34d399",
+            "warning": "#fde047",
+            "error": "#fb7185",
+         },
+          dark: {
+            ...require("daisyui/src/theming/themes")["dark"],
+            "primary": "#fb923c",
+            "secondary": "#7dd3fc",
+            "accent": "#a3e635",
+            "neutral": "#1f2937",
+            "base-100": "#4b5563",
+            "base-300": "#1f2937",
+            "info": "#c084fc",
+            "success": "#34d399",
+            "warning": "#fde047",
+            "error": "#fb7185",
+          },
+        },
+      ],
+    },
 }
-
